a1ebc3c837f2aac27daf831bc9790d6bccbb8ddc
pawan-singh-ai <137815383+pawan-singh-ai@users.noreply.github.com>
2024-10-07 22:13:39 +0530

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,14 @@
+let num=1;
+while (num<50) {
+    
+        if(num%2==0){
+        
+           console.log("number is even",num);
+           
+        }else{
+            console.log("number is odd",num);
+        }    
+        num++;
+    }
+   
+
