c9296092faeb06fcc7b9cc1307f6b68c8eb88cd7
pawan-singh-ai <137815383+pawan-singh-ai@users.noreply.github.com>
2024-10-07 22:47:27 +0530

    testassignment

--- odd.js
+++ odd.js
@@ -1,14 +1,44 @@
-let num=1;
-while (num<50) {
-    
-        if(num%2==0){
-        
-           console.log("number is even",num);
-           
-        }else{
-            console.log("number is odd",num);
-        }    
-        num++;
-    }
-   
+// // question-1
 
+// function number(num){
+//     while (num>0) {
+//         if(num%2==0){
+//            console.log("number is even",num);
+//            break;
+           
+//         }else{
+//             console.log("number is odd",num);
+//             break;
+//         }    
+//     }
+    
+// }
+// number(5)
+
+
+// question-2
+
+// function myfun(marks){
+
+//     if(marks>90 || marks==90){
+//         return "grade A"
+//     }
+//     if(marks>80 || marks==80){
+//         return "grade B"
+//     }
+//     if(marks>70 || marks==70){
+//         return "grade C"
+//     }
+//     if(marks>60 || marks==60){
+//         return "grade D"
+//     }
+//     if(marks>50 || marks==50){
+//         return "grade E"
+//     }   
+//     else{
+//         return "FAIL"
+//     }
+// }
+
+// let rv=myfun(100);
+// console.log(rv);
